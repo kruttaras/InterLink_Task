f4498452e31e2ff3843b50fb070bb8f23e650592
Taras Krutiak <krut.taras@gmail.com>
2019-10-25 17:22:11 +0300

    formating

--- src/main/java/com/okrutiak/service/InternshipService.java
+++ src/main/java/com/okrutiak/service/InternshipService.java
@@ -8,9 +8,10 @@ import java.util.stream.Collectors;
 
 public class InternshipService {
     public Internship createInternshipForStudentsWithAverageLvl(String name, List<Student> students) {
-        double average = students.stream().map(Student::getKnowledge)
-                .mapToInt(i -> i)
-                .average().orElse(0);
+        double average = students.stream()
+            .map(Student::getKnowledge)
+            .mapToInt(i -> i)
+            .average().orElse(0);
         List<Student> enrolledStudents = students.stream()
                 .filter(student -> student.getKnowledge() > average).collect(Collectors.toList());
         return new Internship(name, enrolledStudents);
